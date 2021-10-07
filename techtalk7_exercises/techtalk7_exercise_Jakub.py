7ab22c1902efa6e143e074cdab5626906f25aa05
JakubPonulak <jakub.ponulak@gmail.com>
2021-10-07 10:48:44 -0700

    tech talk 7 - jakub

--- techtalk7_exercises/techtalk7_exercise_Jakub.py
+++ techtalk7_exercises/techtalk7_exercise_Jakub.py
@@ -1,4 +1,8 @@
 student_list = ['pam','rob','joe','greg','bob','amy','matt']
 
-print student_list[2:5]
-print student_list[:-5]
+print(student_list[2:5])
+print(student_list[:-5])
+print(student_list[5:])
+print(student_list[0:6])
+if any('rob' in s for s in student_list):
+    print(student_list[1])
