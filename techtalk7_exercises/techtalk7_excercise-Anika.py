a8e0e58a99fa5f6e97e82e4780e9034d8efb028b
anikasharma <anika1sharma1@gmail.com>
2021-10-11 10:58:09 -0700

    Tech talk assignment, unsigned addition

---
+++ techtalk7_exercises/techtalk7_excercise-Anika.py
@@ -0,0 +1,14 @@
+student_list = ['pam','rob','joe','greg','bob','amy','matt']
+
+# The following print statement includes elements at index 2 & excludes element at index 5
+print(student_list[2:5])
+
+# print elements beginning to 4th
+print(student_list[:-5])
+
+# print elements 6th to end (index 5)
+print
+
+# print elements beginning to end
+
+# check if rob is in the student_list
