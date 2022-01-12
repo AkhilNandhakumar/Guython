810cb78926c95200f1da67b368d2e48ee1078c77
Jakub Ponulak <jakub.ponulak@gmail.com>
2022-01-12 13:13:50 -0800

    testing putting jekyll files in seperate directory

--- main.py
+++ main.py
@@ -314,9 +314,9 @@ def wchecks():
 
 
 # Forums socket.io code
-@socketio.on('testing')
-def handle_message(data):
-    print('received message: ' + data)
+# @socketio.on('testing')
+# def handle_message(data):
+#     print('received message: ' + data)
 
 
 # if __name__ == '__main__':
