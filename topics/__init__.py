42d4a0a7cd918c5c2d819862e0fa6ccd9914dd03
JakubPonulak <jakub.ponulak@gmail.com>
2022-03-24 12:27:42 -0700

    added file for js and approute

--- topics/__init__.py
+++ topics/__init__.py
@@ -11,3 +11,8 @@ app_topics = Blueprint('topics', __name__,
 def scss():
     return render_template("topics/scss.html")
 
+
+@app_topics.route('/javascript/')
+def javascript():
+    return render_template("topics/javascript.html")
+
