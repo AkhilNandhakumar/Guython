508525bbbcbe68cb30b29f48b9875a50d1b6768d
JakubPonulak <jakub.ponulak@gmail.com>
2022-04-06 12:23:00 -0700

    made new branch, added some files

--- __init__.py
+++ __init__.py
@@ -1,6 +1,8 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_login import LoginManager
+
 
 """This needs to be isolated to support blueprints and models"""
 app = Flask(__name__)
