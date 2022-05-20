6a5bbb21f6bce3addb0e938733408c22281f0b2f
somad <somaditya94@gmail.com>
2022-05-19 20:40:17 -0700

    Calender Data Comment

--- student_calendar/cal_model.py
+++ student_calendar/cal_model.py
@@ -73,3 +73,72 @@ class Calendar(UserMixin, db.Model):
         db.session.delete(self)
         db.session.commit()
         return None
+# Tutorial: https://www.sqlalchemy.org/library.html#tutorials, try to get into Python shell and follow along
+
+# Define the Users table within the model
+# -- Object Relational Mapping (ORM) is the key concept of SQLAlchemy
+# -- a.) db.Model is like an inner layer of the onion in ORM
+# -- b.) Users represents data we want to store, something that is built on db.Model
+# -- c.) SQLAlchemy ORM is layer on top of SQLAlchemy Core, then SQLAlchemy engine, SQL# Tutorial: https://www.sqlalchemy.org/library.html#tutorials, try to get into Python shell and follow along
+#
+# # Define the Users table within the model
+# # -- Object Relational Mapping (ORM) is the key concept of SQLAlchemy
+# # -- a.) db.Model is like an inner layer of the onion in ORM
+# # -- b.) Users represents data we want to store, something that is built on db.Model
+# # -- c.) SQLAlchemy ORM is layer on top of SQLAlchemy Core, then SQLAlchemy engine, SQL# Tutorial: https://www.sqlalchemy.org/library.html#tutorials, try to get into Python shell and follow along
+#
+# # Define the Users table within the model
+# # -- Object Relational Mapping (ORM) is the key concept of SQLAlchemy
+# # -- a.) db.Model is like an inner layer of the onion in ORM
+# # -- b.) Users represents data we want to store, something that is built on db.Model
+# # -- c.) SQLAlchemy ORM is layer on top of SQLAlchemy Core, then SQLAlchemy engine, SQL# Tutorial: https://www.sqlalchemy.org/library.html#tutorials, try to get into Python shell and follow along
+#
+# # Define the Users table within the model
+# # -- Object Relational Mapping (ORM) is the key concept of SQLAlchemy
+# # -- a.) db.Model is like an inner layer of the onion in ORM
+# # -- b.) Users represents data we want to store, something that is built on db.Model
+# # -- c.) SQLAlchemy ORM is layer on top of SQLAlchemy Core, then SQLAlchemy engine, SQL# Tutorial: https://www.sqlalchemy.org/library.html#tutorials, try to get into Python shell and follow along
+#
+# # Define the Users table within the model
+# # -- Object Relational Mapping (ORM) is the key concept of SQLAlchemy
+# # -- a.) db.Model is like an inner layer of the onion in ORM
+# # -- b.) Users represents data we want to store, something that is built on db.Model
+# # -- c.) SQLAlchemy ORM is layer on top of SQLAlchemy Core, then SQLAlchemy engine, SQL# Tutorial: https://www.sqlalchemy.org/library.html#tutorials, try to get into Python shell and follow along
+#
+# # Define the Users table within the model
+# # -- Object Relational Mapping (ORM) is the key concept of SQLAlchemy
+# # -- a.) db.Model is like an inner layer of the onion in ORM
+# # -- b.) Users represents data we want to store, something that is built on db.Model
+# # -- c.) SQLAlchemy ORM is layer on top of SQLAlchemy Core, then SQLAlchemy engine, SQL# Tutorial: https://www.sqlalchemy.org/library.html#tutorials, try to get into Python shell and follow along
+#
+# # Define the Users table within the model
+# # -- Object Relational Mapping (ORM) is the key concept of SQLAlchemy
+# # -- a.) db.Model is like an inner layer of the onion in ORM
+# # -- b.) Users represents data we want to store, something that is built on db.Model
+# # -- c.) SQLAlchemy ORM is layer on top of SQLAlchemy Core, then SQLAlchemy engine, SQL# Tutorial: https://www.sqlalchemy.org/library.html#tutorials, try to get into Python shell and follow along
+#
+# # Define the Users table within the model
+# # -- Object Relational Mapping (ORM) is the key concept of SQLAlchemy
+# # -- a.) db.Model is like an inner layer of the onion in ORM
+# # -- b.) Users represents data we want to store, something that is built on db.Model
+# # -- c.) SQLAlchemy ORM is layer on top of SQLAlchemy Core, then SQLAlchemy engine, SQL# Tutorial: https://www.sqlalchemy.org/library.html#tutorials, try to get into Python shell and follow along
+#
+# # Define the Users table within the model
+# # -- Object Relational Mapping (ORM) is the key concept of SQLAlchemy
+# # -- a.) db.Model is like an inner layer of the onion in ORM
+# # -- b.) Users represents data we want to store, something that is built on db.Model
+# # -- c.) SQLAlchemy ORM is layer on top of SQLAlchemy Core, then SQLAlchemy engine, SQL# Tutorial: https://www.sqlalchemy.org/library.html#tutorials, try to get into Python shell and follow along
+#
+# # Define the Users table within the model
+# # -- Object Relational Mapping (ORM) is the key concept of SQLAlchemy
+# # -- a.) db.Model is like an inner layer of the onion in ORM
+# # -- b.) Users represents data we want to store, something that is built on db.Model
+# # -- c.) SQLAlchemy ORM is layer on top of SQLAlchemy Core, then SQLAlchemy engine, SQL
+
+
+
+
+
+
+
+
