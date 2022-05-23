731fd2806d7a43ed8ab55f62ed6a5c5a213d996e
akhil <akhil@nandhakumar.net>
2022-05-23 12:48:59 -0700

    blueprint init for learning zones

---
+++ learning/__init__.py
@@ -0,0 +1,22 @@
+from flask import Blueprint, render_template
+
+app_learning = Blueprint('learning', __name__,
+                       url_prefix='/learning',
+                       template_folder='templates',
+                       static_folder='static',
+                       static_url_path='static')
+
+
+@app_learning.route('/movie/')
+def movies():
+    return render_template("learning/movie.html")
+
+
+@app_learning.route('/starter/')
+def movies():
+    return render_template("learning/starter.html")
+
+
+@app_learning.route('/testprep/')
+def movies():
+    return render_template("learning/testprep.html")
